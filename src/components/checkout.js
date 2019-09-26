539b9d0feb17aa8635c741e26291879a8ca795e2
Joanna Abella <joanna.abella96@gmail.com>
2019-09-26 22:00:15 +0200

    finished vg requirements

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -3,9 +3,24 @@ import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 import avatar from '../undraw_destinations_fpv7 copy.svg'
 import Confirm from '../components/confirm';
+import axios from 'axios';
+
+let API = 'http://localhost:8080/';
 
 function Checkout(props) {
+    const cartItems = props.cart;
+    let totalPrice = 0;
+
+    cartItems.forEach(item => {
+        totalPrice += item.totalPrice;
+    });
+
+    const products = cartItems.map((item) => {
+        return { 'value': { name: item.product.name, totalPrice: item.totalPrice, quantity: item.quantity } };
+    })
+
     const [hasCheckedOut, updatehasCheckedout] = useState(false);
+    const [order, updateOrder] = useState({ name: '', address: '', total_price: totalPrice, products: products });
 
     function clearShoppingCart(event) {
         event.preventDefault();
@@ -16,25 +31,40 @@ function Checkout(props) {
         updatehasCheckedout(true);
     }
 
+    function createOrder(event) {
+        event.preventDefault();
+        let URL = `${API}api/collections/save/Orders`;
+        axios
+            .post(URL, { data: order })
+            .then((response) => {
+                clearShoppingCart(event);
+            })
+    }
+
+    function updateOrderKey(key, event) {
+        order[key] = event.target.value;
+        updateOrder(order);
+    }
+
     return (
         <div className='checkout-container'>
             <img className='checkout-avatar' src={avatar}></img>
             <Helmet>
                 <title>Checkout</title>
             </Helmet>
-            <form className='checkout-form' onSubmit={(event) => clearShoppingCart(event)}>
+            <form className='checkout-form' onSubmit={(event) => createOrder(event)}>
                 {hasCheckedOut ? <Confirm /> : ''}
                 {!hasCheckedOut ?
                     <>
                         <h2 className='checkout-form-title'>Checkout</h2>
                         <label className='checkout-name-label' htmlFor='entername'>För och efternamn<span className='checkout-star'>*</span></label> <br />
-                        <input className='checkout-name-userfield' name='entername' type='text' name='name' spellCheck='false' autoComplete='off' required='required'></input> <br />
+                        <input className='checkout-name-userfield' name='entername' type='text' name='name' spellCheck='false' autoComplete='off' required='required' onChange={(event) => updateOrderKey('name', event)}></input> <br />
 
                         <label className='checkout-email-label' htmlFor='enteremail'>Email<span className='checkout-star'>*</span></label> <br />
-                        <input className='checkout-email-userfield' name='enteremail' type='text' name='email' spellCheck='false' autoComplete='off' required='required'></input> <br />
+                        <input className='checkout-email-userfield' name='enteremail' type='text' name='email' spellCheck='false' autoComplete='off' required='required' onChange={(event) => updateOrderKey('email', event)}></input> <br />
 
                         <label className='checkout-adress-label' htmlFor='enteradress'>Gatuadress<span className='checkout-star'>*</span></label> <br />
-                        <input className='checkout-adress-userfield' name='enteradress' type='text' name='adress' spellCheck='false' autoComplete='off' required='required'></input> <br />
+                        <input className='checkout-adress-userfield' name='enteradress' type='text' name='adress' spellCheck='false' autoComplete='off' required='required' onChange={(event) => updateOrderKey('address', event)}></input> <br />
                         <span className='checkout-error'></span>
 
                         <button className='checkout-button' type='submit'>Genomför köp</button>
--- src/components/reviews.js
+++ src/components/reviews.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 let API = 'http://localhost:8080/';
 
 function createList(review) {
-
     return (
         <li className='single-review' key={review._id}>
             <h5 className='review-title'>{review.title}</h5>
@@ -16,6 +15,7 @@ function createList(review) {
 
 function Reviews({ match }) {
     const [reviews, updateReviews] = useState([]);
+    const [newReview, updateNewReview] = useState({ title: '', body: '', rating: '', product: { _id: match.params.id, link: 'Products', display: match.params.name } });
 
     useEffect(() => {
         axios
@@ -25,8 +25,46 @@ function Reviews({ match }) {
             });
     }, []);
 
+
+    function createReview(event) {
+        event.preventDefault();
+        let URL = `${API}api/collections/save/Reviews`;
+        axios
+            .post(URL, { data: newReview })
+            .then((response) => {
+                updateReviews([...reviews, newReview]);
+                updateNewReview(Object.assign({}, newReview, {
+                    title: '',
+                    body: '',
+                    rating: ''
+                }))
+            })
+    }
+
+    function updateReviewKey(key, event) {
+        const clone = { ...newReview };
+        clone[key] = event.target.value;
+        updateNewReview(clone);
+    }
+
     return (
         <div className='reviews-container'>
+            <form className='review-form' onSubmit={(event) => createReview(event)}>
+                <h2 className='review-form-title'>Omdöme</h2>
+
+                <label className='review-label' htmlFor='entertitle'>Titel<span className='review-star'>*</span></label> <br />
+                <input className='review-userfield' name='entertitle' type='text' spellCheck='false' autoComplete='off' required='required' value={newReview.title} onChange={(event) => updateReviewKey('title', event)}></input> <br />
+
+                <label className='review-label' htmlFor='enterbody'>Kommentar<span className='review-star'>*</span></label> <br />
+                <textarea className='review-textarea' name='enterbody' type='text' spellCheck='false' autoComplete='off' required='required' value={newReview.body} onChange={(event) => updateReviewKey('body', event)}></textarea> <br />
+
+                <label className='review-label' htmlFor='enterrating'>Betyg<span className='review-star'>*</span></label> <br />
+                <input className='review-userfield' name='enterrating' type='number' min='1' max='5' spellCheck='false' autoComplete='off' required='required' value={newReview.rating} onChange={(event) => updateReviewKey('rating', event)}></input> <br />
+
+                <span className='review-error'></span>
+
+                <button className='review-button' type='submit'>Skicka</button>
+            </form>
             <ul className='reviews-list'>
                 {reviews.map((review) => {
                     return createList(review);
